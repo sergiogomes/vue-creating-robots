d78505f20f087546ec4b0273feed8ed1bae76439
sergiogomes <sergiopgomes92@gmail.com>
2020-12-29 12:00:24 -0300

    Creating the header.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" alt="Robot logo" class="logo" />
+            Creating Robots
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
       <HomePage />
     </main>
@@ -31,5 +41,24 @@ main {
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
