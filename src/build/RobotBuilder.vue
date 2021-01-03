0fbc463687bbddffa7e6629129f7a937a19a9d8a
sergiogomes <sergiopgomes92@gmail.com>
2021-01-03 17:55:16 -0300

    Rendering robots added to the cart.

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -75,6 +75,24 @@
       </div>
     </div>
 
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{ robot.head.title }}</td>
+            <td class="cost">{{ robot.cost }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
 </template>
 
@@ -126,6 +144,7 @@ export default {
         + robot.rightArm.cost
         + robot.base.cost;
       this.cart.push({ ...robot, cost });
+      // this.cart.push(Object.assign({}, robot, { cost }));
     },
     selectNextPart(part, group) {
       this.robot[part] = getNextValidIndex(
@@ -250,5 +269,13 @@ export default {
   width: 220px;
   padding: 3px;
   font-size: 16px;
+}
+td, th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost {
+  text-align: right;
 }
 </style>
