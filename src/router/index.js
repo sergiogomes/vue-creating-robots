59c91b1cd124fe46b5c46a1d9c19f0b4245f55a3
sergiogomes <sergiopgomes92@gmail.com>
2021-01-03 21:31:45 -0300

    Creating a Browse Parts component rendered as child route.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 
 Vue.use(Router);
@@ -18,6 +23,33 @@ export default new Router({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          name: 'BrowseHeads',
+          path: 'heads',
+          component: RobotHeads,
+        },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms,
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torsos',
+          component: RobotTorsos,
+        },
+        {
+          name: 'BrowseBases',
+          path: 'bases',
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:id',
