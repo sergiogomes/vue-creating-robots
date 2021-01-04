114be789efe77e920171b608a3fe3197a6a0ebf8
sergiogomes <sergiopgomes92@gmail.com>
2021-01-03 22:39:57 -0300

    Creating getter on store.

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,29 @@
         </tr>
       </tbody>
     </table>
+    <h2 class="sale-items">You saved money on these robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">
+            {{ robot.head.title }}
+          </td>
+          <td class="cost">
+            {{ robot.cost }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
  </div>
 </template>
 
@@ -33,19 +56,27 @@ export default {
     cart() {
       return this.$store.state.cart;
     },
+    cartSaleItems() {
+      return this.$store.getters.cartSaleItems;
+    },
   },
 };
 </script>
 
 <style scoped>
-  td, th {
-    padding: 5px;
-  }
-  .robot-title {
-    text-align: left;
-    padding-right: 200px;
-  }
-  .cost {
-    text-align: right;
-  }
+td, th {
+  padding: 5px;
+}
+.robot-title {
+  text-align: left;
+  padding-right: 200px;
+}
+.cost {
+  text-align: right;
+}
+.sale-items {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
+}
 </style>
